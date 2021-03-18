f1f8ded1650c489c781f969b2248bc95f76b4c6c
KarinaNeuburger <37090706+KarinaNeuburger@users.noreply.github.com>
2021-03-18 08:22:33 -0300

    Excluído linhas de comentários

--- 2D Bit Game Edu/Assets/Scripts/PlayerMovement.cs
+++ 2D Bit Game Edu/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,12 @@ public class PlayerMovement : MonoBehaviour {
 
 	public CharacterController2D controller;
 
-    //public static List<int> questionDestroyer = new List<int>();
-    //public static List<string> questionDestroyer = new List<string>();
-    public static Vector3 playerTeleportPosition = new Vector3(-9.25f, -5.5f, 0);
+	public static Vector3 playerTeleportPosition = new Vector3(-9.25f, -5.5f, 0);
 	public float runSpeed = 40f;
-    private GameObject playerObj = null;
-    private GameObject questionIcon = null;
+	private GameObject playerObj = null;
+  	private GameObject questionIcon = null;
 
-    float horizontalMove = 0f;
+	float horizontalMove = 0f;
 	bool jump = false;
 	bool crouch = false;
 
@@ -31,14 +29,12 @@ public class PlayerMovement : MonoBehaviour {
            
         }
     }
-
-    // Update is called once per frame
+	
     void Update() {
 
         if (CameraMove.moveCamera == true && PlayerTeleport.portalCollision == true)
         {
             playerObj = GameObject.Find("Player");
-            //Vector3 playerTeleportPosition = new Vector3((float)-11.28, (float)-25.6, 0);
             Debug.Log("Teste de movimento do jogador");
             playerObj.transform.position = playerTeleportPosition;
             PlayerTeleport.portalCollision = false;
@@ -54,7 +50,6 @@ public class PlayerMovement : MonoBehaviour {
 
     void FixedUpdate ()
 	{
-		// Move our character
 		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
 		jump = false;
 	}
